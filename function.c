f89e1c9026dad074a71744d55d4fafa801abeb22
deepti652003 <75712513+deepti652003@users.noreply.github.com>
2020-12-14 21:31:04 +0530

    Add files via upload

---
+++ function.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+void myfunctionname()
+{
+    printf("sum=%d",200+600);
+    printf("we are inside the function\n");
+}
+  int main()
+  {
+      myfunctionname();
+  }
